2b5b4f60ed1441f12cbf0d1f432210bab2b0217a
Kevin Mangal <kmangal818@gmail.com>
2019-03-15 12:25:27 -0400

    Optimized load route

--- routes/api/load.js
+++ routes/api/load.js
@@ -7,11 +7,28 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   try {
     const shopConfig = await AppConfig.findOne({shopifyDomain: req.query.shop}).exec();
-    const options = await Option.find({shopifyDomain: req.query.shop}).exec();
-    if (!shopConfig || !options) {
+    const shopOptions = await Option.find({shopifyDomain: req.query.shop}).exec();
+    if (!shopConfig || !shopOptions) {
       return res.status(404).send();
     }
-    res.send({shopConfig, options});
+    const options = shopOptions.map(opt => {return {
+      title: opt.title,
+      collections: opt.collections
+    }});
+    let colors = {};
+    shopOptions.forEach(opt => {
+      opt.colors.forEach(color => {
+        if (colors.hasOwnProperty(color.title)) {
+          colors[color.title].collections.push(opt.title);
+        } else {
+          colors[color.title] = {
+            collections: [opt.title],
+            value: color.value
+          }
+        }
+      });
+    })
+    res.send({shopConfig, options, colors});
   } catch(e) {
     console.log('Error getting app load');
     res.status(400).send(e);
